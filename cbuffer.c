11300b5e14655bba03a9ed5b37148dc31c3d52fe
manhquyencodene <quyenltm05@gmail.com>
2025-07-21 17:06:27 +0700

    tai 2 file code

---
+++ cbuffer.c
@@ -0,0 +1,25 @@
+#include "cbuffer.h"
+
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size) {
+    // Code
+}
+
+void cb_clear(cbuffer_t *cb) {
+    // Code
+}
+
+uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes) {
+    // Code
+}
+
+uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes) {
+    // Code
+}
+
+uint32_t cb_data_count(cbuffer_t *cb) {
+    // Code
+}
+
+uint32_t cb_space_count(cbuffer_t *cb) {
+    // Code
+}
